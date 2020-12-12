1804fefc14fe5cf1b93374f4c2e3813a8d87eabb
SOunit <workbookwork7@gmail.com>
2020-12-12 20:04:04 +0900

    added read range query

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -6,4 +6,40 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+  const minQuery = Artist.find()
+    .sort({ age: 1 })
+    .limit(1)
+    .then((artist) => {
+      return artist[0].age;
+    });
+
+  const maxQuery = Artist.find()
+    .sort({ age: -1 })
+    .limit(1)
+    .then((oldestArtist) => {
+      return oldestArtist[0].age;
+    });
+
+  return Promise.all([minQuery, maxQuery]).then((result) => {
+    return { min: result[0], max: result[1] };
+  });
 };
+
+// usage
+// GetAgeRange().then((argument) => {
+//   console.log(argument); // {min: 13, max: 44}
+// });
+
+// usage
+// Promise.all().then((result) => {
+//   console.log(result);
+// });
+
+// usage
+// Promise.all()
+//   .then((result) => {
+//     return { min: 10, max: 14 };
+//   })
+//   .then((result) => {
+//     console.log(result);
+//   });
